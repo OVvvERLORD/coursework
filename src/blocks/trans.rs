6fafbc53b6021e81f300ac884946a24d07e1d9e1
OVvvERLORD <temy24101962@gmail.com>
2025-03-01 22:57:54 +0300

    super inaccurate transformer2d, but at least it works

--- src/blocks/trans.rs
+++ src/blocks/trans.rs
@@ -33,13 +33,58 @@ impl Transformer2D {
 impl Layer for Transformer2D {
     fn operation(&self, args:(Vec<f32>, Vec<usize>)) -> Result<(Vec<f32>, Vec<usize>), Box<dyn std::error::Error>> {
         let operations = &self.operations;
-        let mut res_vec = args.0;
-        let mut res_vec_shape = args.1;
-        for layer in operations {
-            let (temp_vec, temp_vec_shape) = layer.operation((res_vec.clone(), res_vec_shape.clone()))?;
+        let mut res_vec = args.0.clone();
+        let mut res_vec_shape = args.1.clone();
+
+        for i in 0..operations.len() {
+            if i == 1 {
+                let input_tensor = 
+                ndarray::Array4::from_shape_vec((res_vec_shape[0], res_vec_shape[1], res_vec_shape[2], res_vec_shape[3]), res_vec)
+                .unwrap()
+                .permuted_axes([0, 2, 3, 1])
+                .as_standard_layout()
+                .into_shape_with_order([res_vec_shape[0], res_vec_shape[3] * res_vec_shape[2], res_vec_shape[1]])
+                .unwrap()
+                .as_standard_layout()
+                .to_owned();
+                // inner_dim = res_vec_shape[1]
+                res_vec = input_tensor
+                .as_standard_layout().
+                to_owned().
+                into_raw_vec_and_offset().0;
+                res_vec_shape = vec![res_vec_shape[0], res_vec_shape[3] * res_vec_shape[2], res_vec_shape[1]].to_vec();
+            }
+            if i == 2{
+                if (res_vec_shape.len() == 4 && res_vec_shape[0] == 1) {
+                    res_vec_shape = vec![res_vec_shape[1], res_vec_shape[2], res_vec_shape[3]].to_vec();
+                }
+            }
+            let (temp_vec, temp_vec_shape) = operations[i].operation((res_vec.clone(), res_vec_shape.clone()))?;
             res_vec = temp_vec;
             res_vec_shape = temp_vec_shape;
-        } 
+        }
+        for i in 0..1280 {
+            print!("{:?} ",res_vec[i]);
+        }
+        print!("\n");
+        for i in 1280..1280*2 {
+            print!("{:?} ", res_vec[i]);
+        }
+
+        let mut output = ndarray::Array4::from_shape_vec((res_vec_shape[0], res_vec_shape[1], res_vec_shape[2], res_vec_shape[3]), res_vec)
+        .unwrap()
+        .into_shape_with_order([args.1[0], args.1[2], args.1[3], args.1[1]]) // batches, height, weight, channels (innerd_dim)
+        .unwrap()
+        .as_standard_layout()
+        .to_owned()
+        .permuted_axes([0, 3, 1, 2])
+        .as_standard_layout()
+        .to_owned()
+        ;
+        let residual = ndarray::Array4::from_shape_vec([args.1[0], args.1[1], args.1[2], args.1[3]], args.0).unwrap();
+        output = output + residual;
+        res_vec = output.as_standard_layout().to_owned().into_raw_vec_and_offset().0;
+
         Ok((res_vec, res_vec_shape))
     }
 }
