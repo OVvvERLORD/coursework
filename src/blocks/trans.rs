68f0dea3798a18b3c2cbb8b83a754a43c88cebb9
OVvvERLORD <temy24101962@gmail.com>
2025-03-03 18:28:43 +0300

    trans bug fixed and tested

--- src/blocks/trans.rs
+++ src/blocks/trans.rs
@@ -1,13 +1,15 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
 use crate::{
     layers::{
         layer::Layer,
         norm::GroupNorm,
         linear::Linear,
-        params::Transformer2D_params
+        params::{Transformer2D_params, BasicTransofmerBlock_params}
     },
-    blocks::{
-        btb::BasicTransofmerBlock
-    }
+    blocks::btb::BasicTransofmerBlock,
+    func::functions::input
 };
 
 pub struct Transformer2D {
@@ -21,12 +23,12 @@ impl Transformer2D {
     ) -> Self {
         let mut vec = Vec::<Box<dyn Layer>>::new();
         vec.push(Box::new(GroupNorm {number_of_groups : params.number_of_groups, eps: params.eps, gamma : params.gamma, beta : params.beta}));
-        vec.push(Box::new(Linear {weigths: params.weigths_in.clone(), weights_shape: params.weights_shape_in.clone(), bias: params.bias_in.clone(), bias_shape : params.bias_shape_in.clone(), is_bias : params.is_bias_in.clone()}));
+        vec.push(Box::new(Linear {weigths: params.weigths_in, weights_shape: params.weights_shape_in, bias: params.bias_in, bias_shape : params.bias_shape_in, is_bias : params.is_bias_in}));
         for param in &params.params_for_basics_vec {
             let basic_ins = BasicTransofmerBlock::BasicTransofmerBlock_constr(param);
             vec.push(Box::new(basic_ins));
         }
-        vec.push(Box::new(Linear {weigths: params.weigths_in, weights_shape: params.weights_shape_in, bias: params.bias_in, bias_shape : params.bias_shape_in, is_bias : params.is_bias_in}));
+        vec.push(Box::new(Linear {weigths: params.weigths_out, weights_shape: params.weights_shape_out, bias: params.bias_out, bias_shape : params.bias_shape_out, is_bias : params.is_bias_out}));
         Self { operations: vec ,  number_of_basic : params.params_for_basics_vec.len()}
     }
 }
@@ -85,73 +87,69 @@ impl Layer for Transformer2D {
     }
 }
 
-// #[test]
-// fn test_transformer_small() {
-//     let (test_vec, test_vec_shape) = input(r"C:\study\coursework\src\trash\test_trans_test.safetensors".to_string()).unwrap();
-//     let (encoder_vec, encoder_vec_shape) = input(r"C:\study\coursework\src\trash\test_trans_encoder_test.safetensors".to_string()).unwrap();
-//     print!("{:?}", test_vec_shape);
+#[test]
+fn test_transformer_unbiased() {
+    let (test_vec, test_vec_shape) = input(r"C:\study\coursework\src\trash\test_trans_test.safetensors".to_string()).unwrap();
+    let (encoder_vec, encoder_vec_shape) = input(r"C:\study\coursework\src\trash\test_trans_encoder_test.safetensors".to_string()).unwrap();
+    let mut param_vec = Vec::<BasicTransofmerBlock_params>::new();
+    for i in 0..10 {
+        let (btb1_weigths_1, btb1_weights_shape_1) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn1_q_test.safetensors", i)).unwrap();
+        let (btb1_weigths_2, btb1_weights_shape_2) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn1_k_test.safetensors", i)).unwrap();
+        let (btb1_weigths_3, btb1_weights_shape_3) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn1_v_test.safetensors", i)).unwrap();
+        let (btb1_weigths_4, btb1_weights_shape_4) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn1_out_w_test.safetensors", i)).unwrap(); 
+        let (btb1_bias_4, btb1_bias_shape_4) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn1_out_b_test.safetensors", i)).unwrap(); 
+    
+        let (btb1_weigths_5, btb1_weights_shape_5) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn2_q_test.safetensors", i)).unwrap();
+        let (btb1_weigths_6, btb1_weights_shape_6) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn2_k_test.safetensors", i)).unwrap();
+        let (btb1_weigths_7, btb1_weights_shape_7) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn2_v_test.safetensors", i)).unwrap();
+        let (btb1_weigths_8, btb1_weights_shape_8) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn2_out_w_test.safetensors", i)).unwrap(); 
+        let (btb1_bias_8, btb1_bias_shape_8) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn2_out_b_test.safetensors", i)).unwrap(); 
+    
+        let (btb1_weigths_ff1, btb1_weights_shape_ff1) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_geglu_w_test.safetensors", i)).unwrap();
+        let (btb1_bias_ff1, btb1_bias_shape_ff1) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_geglu_b_test.safetensors", i)).unwrap();
+    
+        let (btb1_weigths_ff2, btb1_weights_shape_ff2) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_ff_w_test.safetensors", i)).unwrap();
+        let (btb1_bias_ff2, btb1_bias_shape_ff2) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_ff_b_test.safetensors", i)).unwrap();
+    
+        let btb1_params = BasicTransofmerBlock_params {
+        eps_1: 1e-05, gamma_1: 1., beta_1: 0., number_1: 1280, 
+        eps_2: 1e-05, gamma_2: 1., beta_2: 0., number_2: 1280,
+        eps_3: 1e-05, gamma_3: 1., beta_3: 0., number_3: 1280,
+        weigths_1: btb1_weigths_1.to_vec(), weights_shape_1: btb1_weights_shape_1.to_vec(), bias_1: btb1_weigths_1.to_vec(), bias_shape_1: btb1_weights_shape_1.to_vec(), is_bias_1: false,
+        weigths_2: btb1_weigths_2.to_vec(), weights_shape_2: btb1_weights_shape_2.to_vec(), bias_2: btb1_weigths_2.to_vec(), bias_shape_2: btb1_weights_shape_2.to_vec(), is_bias_2: false,
+        weigths_3: btb1_weigths_3.to_vec(), weights_shape_3: btb1_weights_shape_3.to_vec(), bias_3: btb1_weigths_3.to_vec(), bias_shape_3: btb1_weights_shape_3.to_vec(), is_bias_3: false,
+        weigths_4: btb1_weigths_4.to_vec(), weights_shape_4: btb1_weights_shape_4.to_vec(), bias_4: btb1_bias_4.to_vec(), bias_shape_4: btb1_bias_shape_4.to_vec(), is_bias_4: true,
+        encoder_hidden_tensor_1: Rc::new(RefCell::new((encoder_vec.to_vec(), encoder_vec_shape.to_vec()))), if_encoder_tensor_1: false, number_of_heads_1: 20,
+    
+        weigths_5: btb1_weigths_5.to_vec(), weights_shape_5: btb1_weights_shape_5.to_vec(), bias_5: btb1_weigths_5.to_vec(), bias_shape_5: btb1_weights_shape_5.to_vec(), is_bias_5: false,
+        weigths_6: btb1_weigths_6.to_vec(), weights_shape_6: btb1_weights_shape_6.to_vec(), bias_6: btb1_weigths_6.to_vec(), bias_shape_6: btb1_weights_shape_6.to_vec(), is_bias_6: false,
+        weigths_7: btb1_weigths_7.to_vec(), weights_shape_7: btb1_weights_shape_7.to_vec(), bias_7: btb1_weigths_7.to_vec(), bias_shape_7: btb1_weights_shape_7.to_vec(), is_bias_7: false,
+        weigths_8: btb1_weigths_8.to_vec(), weights_shape_8: btb1_weights_shape_8.to_vec(), bias_8: btb1_bias_8.to_vec(), bias_shape_8: btb1_bias_shape_8.to_vec(), is_bias_8: true,
+        encoder_hidden_tensor_2: Rc::new(RefCell::new((encoder_vec.to_vec(), encoder_vec_shape.to_vec()))), if_encoder_tensor_2: true, number_of_heads_2: 20,
+    
+        weigths_ff1: btb1_weigths_ff1.to_vec(), weights_shape_ff1: btb1_weights_shape_ff1.to_vec(), bias_ff1: btb1_bias_ff1.to_vec(), bias_shape_ff1: btb1_bias_shape_ff1.to_vec(), is_bias_ff1: true,
+        weigths_ff2: btb1_weigths_ff2.to_vec(), weights_shape_ff2: btb1_weights_shape_ff2.to_vec(), bias_ff2: btb1_bias_ff2.to_vec(), bias_shape_ff2: btb1_bias_shape_ff2.to_vec(), is_bias_ff2: true
+        };
+        param_vec.push(btb1_params);
+    }
+    let (weigths_in,  weights_shape_in) = input(r"C:\study\coursework\src\trash\test_trans_projin_w_test.safetensors".to_string()).unwrap(); 
+    let (bias_in, bias_shape_in) = input(r"C:\study\coursework\src\trash\test_trans_projin_b_test.safetensors".to_string()).unwrap(); 
 
-//     let mut param_vec = Vec::<BasicTransofmerBlock_params>::new();
-//     for i in 0..10 {
-//         let (btb1_weigths_1, btb1_weights_shape_1) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn1_q_test.safetensors", i)).unwrap();
-//         let (btb1_weigths_2, btb1_weights_shape_2) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn1_k_test.safetensors", i)).unwrap();
-//         let (btb1_weigths_3, btb1_weights_shape_3) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn1_v_test.safetensors", i)).unwrap();
-//         let (btb1_weigths_4, btb1_weights_shape_4) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn1_out_w_test.safetensors", i)).unwrap(); 
-//         let (btb1_bias_4, btb1_bias_shape_4) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn1_out_b_test.safetensors", i)).unwrap(); 
-    
-//         let (btb1_weigths_5, btb1_weights_shape_5) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn2_q_test.safetensors", i)).unwrap();
-//         let (btb1_weigths_6, btb1_weights_shape_6) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn2_k_test.safetensors", i)).unwrap();
-//         let (btb1_weigths_7, btb1_weights_shape_7) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn2_v_test.safetensors", i)).unwrap();
-//         let (btb1_weigths_8, btb1_weights_shape_8) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn2_out_w_test.safetensors", i)).unwrap(); 
-//         let (btb1_bias_8, btb1_bias_shape_8) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_attn2_out_b_test.safetensors", i)).unwrap(); 
-    
-//         let (btb1_weigths_ff1, btb1_weights_shape_ff1) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_geglu_w_test.safetensors", i)).unwrap();
-//         let (btb1_bias_ff1, btb1_bias_shape_ff1) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_geglu_b_test.safetensors", i)).unwrap();
-    
-//         let (btb1_weigths_ff2, btb1_weights_shape_ff2) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_ff_w_test.safetensors", i)).unwrap();
-//         let (btb1_bias_ff2, btb1_bias_shape_ff2) = input(format!(r"C:\study\coursework\src\trash\test_trans_{}_ff_b_test.safetensors", i)).unwrap();
-    
-//         let btb1_params = BasicTransofmerBlock_params {
-//         eps_1: 1e-05, gamma_1: 1., beta_1: 0., number_1: 1280, 
-//         eps_2: 1e-05, gamma_2: 1., beta_2: 0., number_2: 1280,
-//         eps_3: 1e-05, gamma_3: 1., beta_3: 0., number_3: 1280,
-//         weigths_1: btb1_weigths_1.to_vec(), weights_shape_1: btb1_weights_shape_1.to_vec(), bias_1: btb1_weigths_1.to_vec(), bias_shape_1: btb1_weights_shape_1.to_vec(), is_bias_1: false,
-//         weigths_2: btb1_weigths_2.to_vec(), weights_shape_2: btb1_weights_shape_2.to_vec(), bias_2: btb1_weigths_2.to_vec(), bias_shape_2: btb1_weights_shape_2.to_vec(), is_bias_2: false,
-//         weigths_3: btb1_weigths_3.to_vec(), weights_shape_3: btb1_weights_shape_3.to_vec(), bias_3: btb1_weigths_3.to_vec(), bias_shape_3: btb1_weights_shape_3.to_vec(), is_bias_3: false,
-//         weigths_4: btb1_weigths_4.to_vec(), weights_shape_4: btb1_weights_shape_4.to_vec(), bias_4: btb1_bias_4.to_vec(), bias_shape_4: btb1_bias_shape_4.to_vec(), is_bias_4: true,
-//         encoder_hidden_tensor_1: Rc::new(RefCell::new((encoder_vec.to_vec(), encoder_vec_shape.to_vec()))), if_encoder_tensor_1: false, number_of_heads_1: 20,
-    
-//         weigths_5: btb1_weigths_5.to_vec(), weights_shape_5: btb1_weights_shape_5.to_vec(), bias_5: btb1_weigths_5.to_vec(), bias_shape_5: btb1_weights_shape_5.to_vec(), is_bias_5: false,
-//         weigths_6: btb1_weigths_6.to_vec(), weights_shape_6: btb1_weights_shape_6.to_vec(), bias_6: btb1_weigths_6.to_vec(), bias_shape_6: btb1_weights_shape_6.to_vec(), is_bias_6: false,
-//         weigths_7: btb1_weigths_7.to_vec(), weights_shape_7: btb1_weights_shape_7.to_vec(), bias_7: btb1_weigths_7.to_vec(), bias_shape_7: btb1_weights_shape_7.to_vec(), is_bias_7: false,
-//         weigths_8: btb1_weigths_8.to_vec(), weights_shape_8: btb1_weights_shape_8.to_vec(), bias_8: btb1_bias_8.to_vec(), bias_shape_8: btb1_bias_shape_8.to_vec(), is_bias_8: true,
-//         encoder_hidden_tensor_2: Rc::new(RefCell::new((encoder_vec.to_vec(), encoder_vec_shape.to_vec()))), if_encoder_tensor_2: true, number_of_heads_2: 20,
-    
-//         weigths_ff1: btb1_weigths_ff1.to_vec(), weights_shape_ff1: btb1_weights_shape_ff1.to_vec(), bias_ff1: btb1_bias_ff1.to_vec(), bias_shape_ff1: btb1_bias_shape_ff1.to_vec(), is_bias_ff1: true,
-//         weigths_ff2: btb1_weigths_ff2.to_vec(), weights_shape_ff2: btb1_weights_shape_ff2.to_vec(), bias_ff2: btb1_bias_ff2.to_vec(), bias_shape_ff2: btb1_bias_shape_ff2.to_vec(), is_bias_ff2: true
-//         };
-//         param_vec.push(btb1_params);
-//     }
-//     let (weigths_in,  weights_shape_in) = input(r"C:\study\coursework\src\trash\test_trans_projin_w_test.safetensors".to_string()).unwrap(); 
-//     let (bias_in, bias_shape_in) = input(r"C:\study\coursework\src\trash\test_trans_projin_b_test.safetensors".to_string()).unwrap(); 
+    let (weigths_out,  weights_shape_out) = input(r"C:\study\coursework\src\trash\test_trans_projout_w_test.safetensors".to_string()).unwrap(); 
+    let (bias_out, bias_shape_out) = input(r"C:\study\coursework\src\trash\test_trans_projout_b_test.safetensors".to_string()).unwrap(); 
 
-//     let (weigths_out,  weights_shape_out) = input(r"C:\study\coursework\src\trash\test_trans_projout_w_test.safetensors".to_string()).unwrap(); 
-//     let (bias_out, bias_shape_out) = input(r"C:\study\coursework\src\trash\test_trans_projout_b_test.safetensors".to_string()).unwrap(); 
+    let params = Transformer2D_params{
+        number_of_groups: 32, eps: 1e-05, gamma: 1., beta: 0.,
+        weigths_in: weigths_in.to_vec(), weights_shape_in: weights_shape_in.to_vec(), bias_in: bias_in.to_vec(), bias_shape_in: bias_shape_in.to_vec(), is_bias_in: true,
+        weigths_out: weigths_out.to_vec(), weights_shape_out: weights_shape_out.to_vec(), bias_out: bias_out.to_vec(), bias_shape_out: bias_shape_out.to_vec(), is_bias_out: true,
+        params_for_basics_vec: param_vec
+    };
+    let trans2d = Transformer2D::Transformer2D_constr(params);
 
-//     let params = Transformer2D_params{
-//         number_of_groups: 32, eps: 1e-05, gamma: 1., beta: 0.,
-//         weigths_in: weigths_in.to_vec(), weights_shape_in: weights_shape_in.to_vec(), bias_in: bias_in.to_vec(), bias_shape_in: bias_shape_in.to_vec(), is_bias_in: true,
-//         weigths_out: weigths_out.to_vec(), weights_shape_out: weights_shape_out.to_vec(), bias_out: bias_out.to_vec(), bias_shape_out: bias_shape_out.to_vec(), is_bias_out: true,
-//         params_for_basics_vec: param_vec
-//     };
-//     for i in 0..1280 {
-//         print!("{:?} ", params.weigths_out[i]);
-//     }
-//     let trans2d = Transformer2D::Transformer2D_constr(params);
-
-//     let (res_vec, res_vec_shape) = trans2d.operation((test_vec.to_vec(), test_vec_shape.to_vec())).unwrap();
-//     let _ = output( r"C:\study\coursework\src\trash\test_trans_rust_test.safetensors".to_string(), res_vec, res_vec_shape);
-//     // for i in 0..40 {
-//     //     print!("{:?} ", res_vec[i]);
-//     // }
-// }
+    let (res_vec, res_vec_shape) = trans2d.operation((test_vec.to_vec(), test_vec_shape.to_vec())).unwrap();
+    let (py_vec, py_vec_shape) = input(r"C:\study\coursework\src\trash\test_trans_output_test.safetensors".to_string()).unwrap();
+    assert!(py_vec_shape.to_vec() == res_vec_shape);
+    for i in 0..py_vec_shape.len() {
+        assert!( (res_vec[i] - py_vec[i]).abs() <= 1e-01 );
+    }
+}
